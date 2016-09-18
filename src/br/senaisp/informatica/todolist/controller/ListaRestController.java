2f536c1532b35268cd3291a396ab17038bd61df6
vmussato <vinicius.mussato@gmail.com>
2016-09-18 12:32:59 -0300

    Adicionado os pacotes dao e controller

---
+++ src/br/senaisp/informatica/todolist/controller/ListaRestController.java
@@ -0,0 +1,47 @@
+package br.senaisp.informatica.todolist.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.senaisp.informatica.todolist.dao.ListaDao;
+import br.senaisp.informatica.todolist.modelo.ItemLista;
+import br.senaisp.informatica.todolist.modelo.Lista;
+
+@RestController
+public class ListaRestController {
+	@Autowired
+	private ListaDao listaDao;
+	
+	@RequestMapping(value="/lista", method=RequestMethod.POST, consumes=MediaType.APPLICATION_JSON_UTF8_VALUE)
+	public ResponseEntity<Lista> inserir(String strLista){
+		 try {
+			JSONObject jsonOb = new JSONObject(strLista);
+			Lista lista = new Lista();
+			lista.setTitulo(jsonOb.getString("titulo"));
+			List<ItemLista> itens = new ArrayList<>();
+			JSONArray arrayItens = jsonOb.getJSONArray("itens");
+			for(int i = 0; i < arrayItens.length(); i++){
+				ItemLista item = new ItemLista();
+				item.setDescricao(arrayItens.getString(i));
+				item.setLista(lista);
+				itens.add(item);
+			}
+			lista.setItens(itens);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			return new 
+					ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+}
